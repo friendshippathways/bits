e3f3854bf45acdbf10ca6708fb8289d90e953f56
friendshippathways <159221519+friendshippathways@users.noreply.github.com>
2024-02-06 21:01:44 -0700

    Add files via upload

---
+++ localize.js
@@ -0,0 +1,65 @@
+<script>
+  // Get the iframe element
+  var iframe = document.getElementById('ytplayeriframe');
+
+  // Get the current src attribute
+  var src = iframe.getAttribute('src');
+
+  // Get the current URL
+  var currentUrl = window.location.href;
+
+  // Check if the URL contains "=ar"
+  if (currentUrl.includes('=ar')) {
+
+    // Replace the cc_lang_pref parameter with "ar"
+    src = src.replace(/cc_lang_pref=[^&]+/, 'cc_lang_pref=ar');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+    // Replace the hl parameter with "ar"
+    src = src.replace(/hl=[^&]+/, 'hl=ar');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+  }
+
+  // Check if the URL contains "=fa"
+  if (currentUrl.includes('=fa')) {
+
+    // Replace the cc_lang_pref parameter with "fa"
+    src = src.replace(/cc_lang_pref=[^&]+/, 'cc_lang_pref=fa');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+    // Replace the hl parameter with "fa"
+    src = src.replace(/hl=[^&]+/, 'hl=fa');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+  }
+
+
+  // Check if the URL contains "=fr"
+  if (currentUrl.includes('=fr')) {
+
+    // Replace the cc_lang_pref parameter with "fr"
+    src = src.replace(/cc_lang_pref=[^&]+/, 'cc_lang_pref=fr');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+    // Replace the hl parameter with "fr"
+    src = src.replace(/hl=[^&]+/, 'hl=fr');
+
+    // Update the src attribute
+    iframe.setAttribute('src', src);
+
+  }
+
+  // Reload the iframe source
+  iframe.src = iframe.src;
+</script>
