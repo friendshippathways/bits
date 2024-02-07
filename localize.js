ff85b40dca97a99b39d8ae99c77fad4dcc109de2
friendshippathways <159221519+friendshippathways@users.noreply.github.com>
2024-02-06 22:04:59 -0700

    Add files via upload

--- localize.js
+++ localize.js
@@ -1,47 +1,38 @@
 window.onload = function() {
-    // Your script logic here
 
-// Get the iframe element
-var iframe = document.getElementById('ytplayeriframe');
-if (iframe) { // Ensure the iframe exists
-    // Get the current src attribute
-    var src = iframe.getAttribute('src');
+	// Get the iframe element
+	var iframe = document.getElementById('ytplayeriframe');
+	if (iframe) { // Ensure the iframe exists
+		// Get the current src attribute
+		var src = iframe.getAttribute('src');
 
-    // Define an object with language codes as keys
-    var languages = {
-        'ar': 'Arabic',
-        'fa': 'Farsi',
-        'fr': 'French',
-        'id': 'Indonesian',
-        'tr': 'Filipino',
-        // Add more languages as needed
-    };
+		// Define an object with language codes as keys
+		var languages = {
+			'ar': 'Arabic',
+			'fa': 'Farsi',
+			'fr': 'French',
+			'id': 'Indonesian',
+			'tr': 'Filipino',
+			// Add more languages as needed
+		};
 
-    // Get the current URL
-    var currentUrl = window.location.href;
+	// Get the current URL
+	var currentUrl = window.location.href;
 
-    // Iterate over each language code
-    Object.keys(languages).forEach(function(langCode) {
-        if (currentUrl.includes('=' + langCode)) {
-            // Alert the found language code for testing
-            //alert('Language code found: ' + langCode);
+	// Iterate over each language code
+	Object.keys(languages).forEach(function(langCode) {
+		if (currentUrl.includes('=' + langCode)) {
 
-            // Alert the original src for testing
-            //alert('src before replace: ' + src);
+			// Replace the cc_lang_pref and hl parameters
+			src = src.replace(/(cc_lang_pref=)[^&]+/, '$1' + langCode);
+			src = src.replace(/(hl=)[^&]+/, '$1' + langCode);
 
-            // Replace the cc_lang_pref and hl parameters
-            src = src.replace(/(cc_lang_pref=)[^&]+/, '$1' + langCode);
-            src = src.replace(/(hl=)[^&]+/, '$1' + langCode);
-
-            // Alert the replaces src for testing
-            //alert('src after replace: ' + src);
-
-            // Update the iframe src attribute once for the found language code
-            iframe.setAttribute('src', src);
+			// Update the iframe src attribute once for the found language code
+			iframe.setAttribute('src', src);
             
-       		// After setting the src
+			// After setting the src
 			iframe.style.display = 'block';
-        }
-    });
-}
+			}
+		});
+	}
 };
