b8c7b0b96dd9f63f450ce0759daa0a3f5c12698c
friendshippathways <159221519+friendshippathways@users.noreply.github.com>
2024-02-06 21:32:03 -0700

    Add files via upload

--- localize.js
+++ localize.js
@@ -23,9 +23,15 @@ if (iframe) { // Ensure the iframe exists
             // Alert the found language code for testing
             alert('Language code found: ' + langCode);
 
+            // Alert the original src for testing
+            alert('src before replace: ' + src);
+
             // Replace the cc_lang_pref and hl parameters
             src = src.replace(/(cc_lang_pref=)[^&]+/, '$1' + langCode);
             src = src.replace(/(hl=)[^&]+/, '$1' + langCode);
+
+            // Alert the replaces src for testing
+            alert('src after replace: ' + src);
 
             // Update the iframe src attribute once for the found language code
             iframe.setAttribute('src', src);
